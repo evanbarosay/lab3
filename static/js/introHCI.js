9f5446bc6cee3b165b8e8aea927f07ad35d02f26
Evan Barosay <evanbarosay@gmail.com>
2018-01-25 10:49:28 -0800

    lab 3

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,8 +10,34 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Javascript is ready to go");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").addClass("active");
+		$("a.thumbnail").click(projectClick);
+
 	});
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).css("background-color", "#7fff00");
+    //var containingProject = $(this).closest(".project");
+    /*var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       description.html("<p>This green is a little hard on the eyes, isn't it? "  + "</p>");*/
+    $(".project").click(function(){
+    	$(".project p").toggle();
+    })
+    	$("#myelement").fadeIn()
+
+
+    }
+
+
+
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
